173f1b093540f5cf1ad0ae63672984dc81d01c3d
email <nednadima@bonaparteimpress.com>
2013-08-16 14:01:44 -0400

    Some old files

---
+++ Richdad/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Richdad::Application.config.secret_key_base = '89c2c61f4c76cfb6e5c3b9683809b5ffb8616bf90cf0e74cd1ea46f0a6bc3e01dc15006ff243fd4509bf51b2058bf2cbc23f366c3bd853627eb937680835ed08'
---
+++ Richdad/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Richdad::Application.config.session_store :cookie_store, key: '_Richdad_session'
