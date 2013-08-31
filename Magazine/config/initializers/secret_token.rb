937d8a3ad28e806d7c1793c4294089b9e2edd6f6
email <nednadima@bonaparteimpress.com>
2013-08-30 23:13:52 -0400

    every new additions

---
+++ Magazine/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Magazine::Application.config.secret_key_base = '36b0a4a46bd4ba563bef01c07d381f37f8109296563a11d15d538b564d753a70ca63c413f0a73335e69c9195e32fcdef49777cbf20bb17c58fc9545c88023985'
